a628c1de740fd07587b49e3714c829e62b9a997a
nhkim <nhkim1529@gmail.com>
2023-08-11 00:02:21 +0900

    Feat: 12.가게 상세페이지 Adapter, Fragment, Item 작성

--- app/src/main/java/com/devinsight/vegiedo/view/store/StoreDetailPageAdapter.java
+++ app/src/main/java/com/devinsight/vegiedo/view/store/StoreDetailPageAdapter.java
@@ -1,92 +1,98 @@
 package com.devinsight.vegiedo.view.store;
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.devinsight.vegiedo.R;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class StoreDetailPageAdapter extends RecyclerView.Adapter<StoreDetailPageAdapter.CustomViewHolder> {
+public class StoreDetailPageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    //Item들을 담을 배열 list 생성
-    private ArrayList<StoreDetailData> arrayList;
+    private List<Item> itemList;
 
-    public StoreDetailPageAdapter(ArrayList<StoreDetailData> arrayList) {
-        this.arrayList = arrayList;
+    public StoreDetailPageAdapter(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return itemList.get(position).getItemType().ordinal();
     }
 
     @NonNull
     @Override
-    // viewType 형태의 아이템 뷰를 위한 뷰홀더 객체 생성 =>화면 출력 최초에 생성될 때 생명주기를 의미
-    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.store_detail_item, parent, false);
-        CustomViewHolder holder = new CustomViewHolder(view);
-        return holder;
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        if (viewType == Item.ItemType.STORE_DETAIL_PAGE.ordinal()){
+            View view = inflater.inflate(R.layout.store_detail_item, parent, false);
+            return new ViewHolderTypeThree(view);
+        }
+        return null;
     }
 
     @Override
-    //position(위치)에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
-    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
-        holder.review_photo.setImageResource(arrayList.get(position).getIv_profile());
-        holder.reviewer_name.setText(arrayList.get(position).getTv_name());
-        holder.review.setText(arrayList.get(position).getTv_content());
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Item item = itemList.get(position);
+        if (holder instanceof ViewHolderTypeThree) {
+            ((ViewHolderTypeThree) holder).title.setText(item.getTitle());
+            ((ViewHolderTypeThree) holder).imageView1.setImageResource(item.getImageResource1());
+            ((ViewHolderTypeThree) holder).imageView2.setImageResource(item.getImageResource2());
+            ((ViewHolderTypeThree) holder).imageView3.setImageResource(item.getImageResource3());
+            ((ViewHolderTypeThree) holder).imageView4.setImageResource(item.getImageResource4());
+            ((ViewHolderTypeThree) holder).imageView5.setImageResource(item.getImageResource5());
+            ((ViewHolderTypeThree) holder).star1.setImageResource(item.getImageResource6());
+            ((ViewHolderTypeThree) holder).star2.setImageResource(item.getImageResource7());
+            ((ViewHolderTypeThree) holder).star3.setImageResource(item.getImageResource8());
+            ((ViewHolderTypeThree) holder).star4.setImageResource(item.getImageResource9());
+            ((ViewHolderTypeThree) holder).star5.setImageResource(item.getImageResource10());
 
-        holder.itemView.setTag(position);
-        //viewHolder 클릭 시 이벤트 구현
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String curName = holder.reviewer_name.getText().toString();
-                Toast.makeText(view.getContext(), "curName", Toast.LENGTH_SHORT).show();
-            }
-        });
-        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                remove(holder.getAdapterPosition());
-                return true;
-            }
-        });
+        }
     }
 
     @Override
-    //전체 아이템 갯수 리턴
     public int getItemCount() {
-        return (null != arrayList ? arrayList.size() : 0);
+        return itemList.size();
     }
 
-    public void remove(int position) {
-        try {
-            arrayList.remove(position);
-            notifyItemRemoved(position);
-        } catch (IndexOutOfBoundsException ex) {
-            ex.printStackTrace();
-        }
-    }
 
-    public class CustomViewHolder extends RecyclerView.ViewHolder {
+    static class ViewHolderTypeThree extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView content;
+        ImageView imageView1;
+        ImageView imageView2;
+        ImageView imageView3;
+        ImageView imageView4;
+        ImageView imageView5;
+        ImageView star1;
+        ImageView star2;
+        ImageView star3;
+        ImageView star4;
+        ImageView star5;
 
-        //
-        protected ImageView review_photo;
-        protected TextView reviewer_name;
-        protected TextView review;
 
-        public CustomViewHolder(@NonNull View itemView) {
+        ViewHolderTypeThree(@NonNull View itemView) {
             super(itemView);
-            this.review_photo = (ImageView) itemView.findViewById(R.id.review_photo1);
-            this.reviewer_name = (TextView) itemView.findViewById(R.id.reviewer_name);
-            this.review = (TextView) itemView.findViewById(R.id.review);
+            title = itemView.findViewById(R.id.title3);
+            content = itemView.findViewById(R.id.content3);
+            imageView1 = itemView.findViewById(R.id.store_image1);
+            imageView2 = itemView.findViewById(R.id.store_image2);
+            imageView3 = itemView.findViewById(R.id.store_image3);
+            imageView4 = itemView.findViewById(R.id.store_image4);
+            imageView5 = itemView.findViewById(R.id.store_image5);
+            star1 = itemView.findViewById(R.id.star1);
+            star2 = itemView.findViewById(R.id.star2);
+            star3 = itemView.findViewById(R.id.star3);
+            star4 = itemView.findViewById(R.id.star4);
+            star5 = itemView.findViewById(R.id.star5);
 
         }
     }
-
 }
--- app/src/main/java/com/devinsight/vegiedo/view/store/StoreDetailPageFragment.java
+++ app/src/main/java/com/devinsight/vegiedo/view/store/StoreDetailPageFragment.java
@@ -2,19 +2,68 @@ package com.devinsight.vegiedo.view.store;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.devinsight.vegiedo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StoreDetailPageFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_store_detail_page, container, false);
+    private static final int ITEMS_COUNT = 10;
 
-        return rootView;
+    private RecyclerView recyclerView;
+    private StoreDetailPageAdapter adapter;
+    private List<Item> items;
+
+    public StoreDetailPageFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_store_detail_page, container, false);
+
+        initializeComponents(view);
+        populateData();
+        setupRecyclerView();
+
+        return view;
+    }
+
+    private void initializeComponents(View view) {
+        recyclerView = view.findViewById(R.id.store_recycle_view);
+    }
+
+    private void populateData() {
+        items = new ArrayList<>();
+        for (int i = 0; i < ITEMS_COUNT; i++) {
+            items.add(createItem(i));
+        }
+    }
+
+    private Item createItem(int index) {
+        return new Item(Item.ItemType.STORE_DETAIL_PAGE, "Title " + index,
+                R.drawable.full_star, R.drawable.full_star, R.drawable.full_star, R.drawable.full_star, R.drawable.empty_star,
+                "Description 1", R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background);
+    }
+
+    private void setupRecyclerView() {
+        recyclerView.setNestedScrollingEnabled(false);
+
+        adapter = new StoreDetailPageAdapter(items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
     }
 }
