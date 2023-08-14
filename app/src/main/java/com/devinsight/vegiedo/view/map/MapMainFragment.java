55cf0cc8bec08ea26181a9f2ad15ba18443f924d
nhkim <nhkim1529@gmail.com>
2023-08-14 19:50:35 +0900

    Feat: 지도 기능 추가

--- app/src/main/java/com/devinsight/vegiedo/view/map/MapMainFragment.java
+++ app/src/main/java/com/devinsight/vegiedo/view/map/MapMainFragment.java
@@ -1,21 +1,32 @@
 package com.devinsight.vegiedo.view.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
 import com.devinsight.vegiedo.R;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class MapMainFragment extends Fragment {
+public class MapMainFragment extends Fragment implements OnMapReadyCallback {
 
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
+    private MapView mapView;
+    private NaverMap naverMap;
+    private static final int REQUEST_LOCATION_PERMISSION = 1000;
+    private FusedLocationSource locationSource;
+    private Marker currentLocationMarker;
 
     private String mParam1;
     private String mParam2;
@@ -27,25 +38,58 @@ public class MapMainFragment extends Fragment {
     public static MapMainFragment newInstance(String param1, String param2) {
         MapMainFragment fragment = new MapMainFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_map, container, false);
+
+        mapView = view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        locationSource = new FusedLocationSource(this, REQUEST_LOCATION_PERMISSION);
+        currentLocationMarker = new Marker();
+
+        requestLocationPermission();
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
+        naverMap.addOnLocationChangeListener(location -> {
+            LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+            currentLocationMarker.setPosition(currentLatLng);
+            currentLocationMarker.setMap(naverMap);
+        });
+    }
+
+    private void requestLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) {
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
