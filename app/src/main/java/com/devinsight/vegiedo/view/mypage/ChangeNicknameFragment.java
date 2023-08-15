97eead362ee42f6735e9b0088689a6acde9d08f7
nhkim <nhkim1529@gmail.com>
2023-08-16 01:02:52 +0900

    Feat: 마이페이지 안 페이지 이동 기능 구현

---
+++ app/src/main/java/com/devinsight/vegiedo/view/mypage/ChangeNicknameFragment.java
@@ -0,0 +1,21 @@
+package com.devinsight.vegiedo.view.mypage;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.devinsight.vegiedo.R;
+
+public class ChangeNicknameFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_change_nickname, container, false);
+    }
+}
