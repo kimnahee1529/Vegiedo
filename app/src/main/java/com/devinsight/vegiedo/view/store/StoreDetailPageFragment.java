03815c633bafa55518fd8b551dc854548cc7c9ad
nhkim <nhkim1529@gmail.com>
2023-08-29 17:53:15 +0900

    Feat: 임시 store api로 StoreApiService로 가게 정보 불러오기

--- app/src/main/java/com/devinsight/vegiedo/view/store/StoreDetailPageFragment.java
+++ app/src/main/java/com/devinsight/vegiedo/view/store/StoreDetailPageFragment.java
@@ -8,17 +8,30 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.devinsight.vegiedo.R;
+import com.devinsight.vegiedo.data.response.MapInquiryResponseDTO;
+import com.devinsight.vegiedo.data.response.StoreInquiryResponseDTO;
+import com.devinsight.vegiedo.service.api.MapApiService;
+import com.devinsight.vegiedo.service.api.StoreApiService;
+import com.devinsight.vegiedo.utill.RetrofitClient;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class StoreDetailPageFragment extends Fragment {
 
+    StoreApiService storeApiService = RetrofitClient.getStoreApiService();
     private static final int ITEMS_COUNT = 10;
 
     private RecyclerView recyclerView;
@@ -38,6 +51,7 @@ public class StoreDetailPageFragment extends Fragment {
         initializeComponents(view);
         populateData();
         setupRecyclerView();
+        loadStoreData();
 
         return view;
     }
@@ -66,4 +80,42 @@ public class StoreDetailPageFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
     }
+
+    private void loadStoreData() {
+        Log.d("API", "loadStoreData안");
+
+        for (long i = 1; i <= 2; i++) {
+            final long storeId = i;
+
+            storeApiService.readStore(storeId).enqueue(new Callback<StoreInquiryResponseDTO>() {
+                @Override
+                public void onResponse(Call<StoreInquiryResponseDTO> call, Response<StoreInquiryResponseDTO> response) {
+                    if (response.isSuccessful()) {
+                        Log.d("API ID:" + storeId, "주소:" + response.body().getAddress());
+                        Log.d("API ID:" + storeId, "사진:" + response.body().getImages());
+                        Log.d("API ID:" + storeId, "태그:" + response.body().getTags());
+                        Log.d("API ID:" + storeId, "위도:" + response.body().getLatitude());
+                        Log.d("API ID:" + storeId, "경도:" + response.body().getLongitude());
+                        Log.d("API ID:" + storeId, "별점:" + response.body().getStars());
+                    } else {
+                        // API 응답이 오류 상태일 때
+                        Log.e("APIError", "Error Code: " + response.code() + ", Message: " + response.message());
+                        try {
+                            Log.e("APIErrorBody", "Error Body: " + response.errorBody().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<StoreInquiryResponseDTO> call, Throwable t) {
+                    // API 호출 자체가 실패했을 때 (예: 인터넷 연결 문제, 시간 초과 등)
+                    Log.e("APIFailure", "Call Failed for storeId " + storeId + ": " + t.getMessage(), t);
+                }
+            });
+        }
+    }
+
+
 }
