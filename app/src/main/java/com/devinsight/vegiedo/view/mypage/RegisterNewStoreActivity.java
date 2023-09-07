3033f8ed02d02c2157a6d1b2cff73629fed6ce7c
nhkim <nhkim1529@gmail.com>
2023-09-07 17:33:19 +0900

    Feat: 뒤로 가기 기능 추가

--- app/src/main/java/com/devinsight/vegiedo/view/mypage/RegisterNewStoreActivity.java
+++ app/src/main/java/com/devinsight/vegiedo/view/mypage/RegisterNewStoreActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +22,18 @@ import android.widget.Toast;
 
 import com.devinsight.vegiedo.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class RegisterNewStoreActivity extends AppCompatActivity {
 
     private EditText mEtAddress;
@@ -28,6 +41,13 @@ public class RegisterNewStoreActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_PICK = 101;
     private ImageView storeImage;
     private ImageView plusBtn;
+    private TextView btnCheckAddress;
+    private ImageView backwardBtn;
+
+    //Geocoding 사용
+    private OkHttpClient client = new OkHttpClient();
+    private String client_id = "g3rcpfm3dn";
+    private String client_secret = "V7Kfjw9sRIutvedYLPpXbCRpOo9ysiFgnSWuhBin";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +79,14 @@ public class RegisterNewStoreActivity extends AppCompatActivity {
         });
 
 
+        backwardBtn = findViewById(R.id.backward_btn);
+        backwardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();  // 현재 액티비티를 종료하고 이전 액티비티로 돌아갑니다.
+            }
+        });
+
         //주소 입력
         mEtAddress = findViewById(R.id.address_edit_text);
         mEtAddress.setFocusable(false);
@@ -88,7 +116,69 @@ public class RegisterNewStoreActivity extends AppCompatActivity {
             }
         });
 
+        btnCheckAddress = findViewById(R.id.StoreRegister_compelete_btn);
+        btnCheckAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String address = mEtAddress.getText().toString().trim();
+                if(!address.isEmpty()) {
+                    fetchLatLngFromAddress(address);
+                } else {
+                    // 주소가 비어있는 경우에 대한 처리 (예: Toast 메시지 표시)
+                    Toast.makeText(RegisterNewStoreActivity.this, "주소를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+    //Geocoding : 주소로 위도, 경도 구하기
+    private void fetchLatLngFromAddress(String address) {
+        String apiUrl = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=" + address;
+
+        Request request = new Request.Builder()
+                .url(apiUrl)
+                .addHeader("X-NCP-APIGW-API-KEY-ID", client_id)
+                .addHeader("X-NCP-APIGW-API-KEY", client_secret)
+                .build();
+
+        client.newCall(request).enqueue(new okhttp3.Callback() {
+
+            @Override
+            public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Log.d("LOGAPI", "onRes 성공");
+                    String responseData = response.body().string();
+                    try {
+                        // JSON 객체 생성
+                        JSONObject jsonObject = new JSONObject(responseData);
+                        JSONObject firstAddress = jsonObject.getJSONArray("addresses").getJSONObject(0);
+
+                        String x = firstAddress.getString("x");
+                        String y = firstAddress.getString("y");
+
+                        Log.d("APIJSONParsing", "경도: " + x + ", 위도: " + y);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.e("APIError", "Error Code: " + response.code() + ", Message: " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+                // Handle the error
+                Log.d("LOGAPI", "onFail 안");
+                e.printStackTrace();
+                runOnUiThread(() -> {
+                    Toast.makeText(RegisterNewStoreActivity.this, "위도, 경도 변환 실패", Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
+    }
+
+
 
     // 화면으로부터 결과값을 받아오는 로직을 만드는 것
     private final ActivityResultLauncher<Intent> getSearchResult = registerForActivityResult(
--- app/src/main/java/com/devinsight/vegiedo/view/mypage/StampBookFragment.java
+++ app/src/main/java/com/devinsight/vegiedo/view/mypage/StampBookFragment.java
@@ -4,17 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 import com.devinsight.vegiedo.R;
 
 public class StampBookFragment extends Fragment {
-
+    private ImageView backwardBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stamp_book, container, false);
+        View view = inflater.inflate(R.layout.fragment_stamp_book, container, false);
+        backwardBtn = view.findViewById(R.id.backward_btn);
+        backwardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getFragmentManager() != null) {
+                    getFragmentManager().popBackStack();
+                }
+            }
+        });
+
+        return view;
     }
 }
