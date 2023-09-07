2531519b9ab6469e3e5b469a6d6989a7f35f6142
nhkim <nhkim1529@gmail.com>
2023-09-07 10:07:11 +0900

    Feat: 가게 상세페이지의 수정/삭제/신고 버튼 기능

--- app/src/main/java/com/devinsight/vegiedo/view/store/UserReviewItemAdapter.java
+++ app/src/main/java/com/devinsight/vegiedo/view/store/UserReviewItemAdapter.java
@@ -1,13 +1,21 @@
 package com.devinsight.vegiedo.view.store;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.devinsight.vegiedo.R;
@@ -16,6 +24,11 @@ import java.util.List;
 
 public class UserReviewItemAdapter extends RecyclerView.Adapter<UserReviewItemAdapter.BaseViewHolder> {
 
+    private enum DialogType {
+        DELETE,
+        REPORT_TYPE,
+        REPORT_COMPELETE
+    }
     private List<UserReviewItem> userReviewItemList;
 
     public UserReviewItemAdapter(List<UserReviewItem> userReviewItemList) {
@@ -57,6 +70,24 @@ public class UserReviewItemAdapter extends RecyclerView.Adapter<UserReviewItemAd
             viewHolder.imageView4.setImageResource(Integer.parseInt(userReviewItem.getUserReviewImageUrlList().get(3)));
             viewHolder.imageView5.setImageResource(Integer.parseInt(userReviewItem.getUserReviewImageUrlList().get(4)));
             viewHolder.ratingBar.setRating(userReviewItem.getRatingBar());
+            viewHolder.storeReview_modify_text.setOnClickListener(v -> {
+                WritingReviewFragment fragment = new WritingReviewFragment();
+                FragmentManager fragmentManager = ((FragmentActivity) viewHolder.itemView.getContext()).getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                // 액티비티의 루트 뷰를 교체(예: android.R.id.content)
+                fragmentTransaction.replace(android.R.id.content, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            });
+            viewHolder.storeReview_delete_text.setOnClickListener(v -> {
+                Log.d("LOG삭제", "삭제 버튼 눌림");
+                showDialog(DialogType.DELETE, holder.itemView.getContext());
+            });
+            viewHolder.storeReview_report_btn.setOnClickListener(v -> {
+                Log.d("LOG신고", "신고 버튼 눌림");
+                showDialog(DialogType.REPORT_TYPE, holder.itemView.getContext());
+            });
 
         } else if (holder instanceof ViewHolderBlogReviewPage) {
             ViewHolderBlogReviewPage viewHolder = (ViewHolderBlogReviewPage) holder;
@@ -64,6 +95,9 @@ public class UserReviewItemAdapter extends RecyclerView.Adapter<UserReviewItemAd
             viewHolder.content.setText(userReviewItem.getContent());
             viewHolder.imageView1.setImageResource(Integer.parseInt(userReviewItem.getUserReviewImageUrlList().get(0)));
         }
+
+
+
     }
 
     @Override
@@ -86,9 +120,13 @@ public class UserReviewItemAdapter extends RecyclerView.Adapter<UserReviewItemAd
         ImageView imageView4;
         ImageView imageView5;
         RatingBar ratingBar;
+        TextView storeReview_modify_text;
+        TextView storeReview_delete_text;
+        ImageView storeReview_report_btn;
 
         ViewHolderReviewPage(@NonNull View itemView) {
             super(itemView);
+
             title = itemView.findViewById(R.id.storeReview_title);
             content = itemView.findViewById(R.id.storeReview_content);
             imageView1 = itemView.findViewById(R.id.storeReview_image1);
@@ -97,7 +135,11 @@ public class UserReviewItemAdapter extends RecyclerView.Adapter<UserReviewItemAd
             imageView4 = itemView.findViewById(R.id.storeReview_image4);
             imageView5 = itemView.findViewById(R.id.storeReview_image5);
             ratingBar = itemView.findViewById(R.id.storeReview_ratingbar);
+            storeReview_modify_text = itemView.findViewById(R.id.storeReview_modify_text);
+            storeReview_delete_text = itemView.findViewById(R.id.storeReview_delete_text);
+            storeReview_report_btn = itemView.findViewById(R.id.storeReview_report_btn);
         }
+
     }
 
     static class ViewHolderBlogReviewPage extends BaseViewHolder {
@@ -111,6 +153,62 @@ public class UserReviewItemAdapter extends RecyclerView.Adapter<UserReviewItemAd
             content = itemView.findViewById(R.id.storeReview_content);
             imageView1 = itemView.findViewById(R.id.storeReview_image1);
         }
+    }
+
+    private void showDialog(DialogType type, Context context) {
+        int layoutId;
+        if(type == DialogType.DELETE){
+            layoutId = R.layout.delete_review_dialog;
+
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View dialogView = inflater.inflate(layoutId, null);
+
+            Button yesBtn = dialogView.findViewById(R.id.DeleteReivew_yesBtn);
+            Button noBtn = dialogView.findViewById(R.id.DeleteReivew_noBtn);
+            ImageView closeIcon = dialogView.findViewById(R.id.green_x_circle);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setView(dialogView);
+
+            final AlertDialog dialog = builder.create();
+
+            yesBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, "예 버튼입니다.", Toast.LENGTH_SHORT).show();
+                    Log.d("LOGf리뷰삭제", "예");
+                    dialog.dismiss();
+                }
+            });
+
+            noBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, "아니오 버튼입니다.", Toast.LENGTH_SHORT).show();
+                    Log.d("LOGf리뷰삭제", "아니오");
+                    dialog.dismiss();
+                }
+            });
+
+            closeIcon.setOnClickListener(v -> dialog.dismiss());
+            dialog.show();
+
+        } else if(type == DialogType.REPORT_TYPE){
+            layoutId = R.layout.select_reporting_type_dialog;
+        } else {
+            layoutId = R.layout.accept_reporting_dialog;
+        }
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View dialogView = inflater.inflate(layoutId, null);
+        ImageView closeIcon = dialogView.findViewById(R.id.green_x_circle);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setView(dialogView);
+
+        final AlertDialog dialog = builder.create();
+        closeIcon.setOnClickListener(v -> dialog.dismiss());
+        dialog.show();
     }
 
 }
