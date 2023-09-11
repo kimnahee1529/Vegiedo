22357d6acb33e3200a39bd8f922ce8ce7fa77428
nhkim <nhkim1529@gmail.com>
2023-09-11 16:45:33 +0900

    Feat: 리뷰 수정/작성 구별

--- app/src/main/java/com/devinsight/vegiedo/view/store/WritingReviewFragment.java
+++ app/src/main/java/com/devinsight/vegiedo/view/store/WritingReviewFragment.java
@@ -25,6 +25,7 @@ import com.bumptech.glide.Glide;
 import com.devinsight.vegiedo.R;
 import com.devinsight.vegiedo.data.request.PostRegisterRequestDTO;
 import com.devinsight.vegiedo.data.request.ReviewModifyrRequestDTO;
+import com.devinsight.vegiedo.data.request.ReviewRegisterRequestDTO;
 import com.devinsight.vegiedo.view.community.GeneralPostFragment;
 import com.devinsight.vegiedo.view.community.PopuralPostFragment;
 import com.devinsight.vegiedo.view.community.WritingFragment;
@@ -53,18 +54,32 @@ public class WritingReviewFragment extends Fragment {
     private ImageView imageView5;
     private List<String> selectedImageUris = new ArrayList<>();
     ActivityViewModel viewModel;
+    private static boolean isModify;
     public WritingReviewFragment() {
         // Required empty public constructor
     }
 
+    //수정 버튼을 눌렀을 때
     public static WritingReviewFragment newInstance(Long storeId, Long reviewId, String content, List<String> imageUrls, float ratingBar) {
         WritingReviewFragment fragment = new WritingReviewFragment();
+        isModify = true;
         Bundle args = new Bundle();
         args.putLong(ARG_STORE_ID, storeId);
         args.putLong(ARG_REVIEW_ID, reviewId);
         args.putString(ARG_CONTENT, content);
         // imageUrls는 ArrayList로 변환하여 넣는다.
         args.putStringArrayList("imageUrls", new ArrayList<>(imageUrls));
+        args.putFloat("ratingBar", ratingBar);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    //리뷰 작성하기 버튼을 눌렀을 때
+    public static WritingReviewFragment newInstance(Long storeId, float ratingBar) {
+        WritingReviewFragment fragment = new WritingReviewFragment();
+        isModify = false;
+        Bundle args = new Bundle();
+        args.putLong(ARG_STORE_ID, storeId);
         args.putFloat("ratingBar", ratingBar);
         fragment.setArguments(args);
         return fragment;
@@ -82,16 +97,6 @@ public class WritingReviewFragment extends Fragment {
 
         viewModel = new ViewModelProvider(requireActivity()).get(ActivityViewModel.class);
 
-        boolean isEditing = false;
-        if(getArguments() != null) {
-            isEditing = getArguments().getBoolean("isEditing", false);
-            if(isEditing) {
-                Long reviewId = getArguments().getLong("reviewId");
-                Long storeId = getArguments().getLong("storeId");
-                // 서버에서 reviewId에 해당하는 데이터를 가져오는 로직을 여기에 구현하세요.
-
-            }
-        }
         initializeViews(view);
         showReviewValues();
         setContentTextWatcher();
@@ -197,14 +202,17 @@ public class WritingReviewFragment extends Fragment {
             Log.d("LOGAPI수정됐나??", ""+contentText+", "+ updatedRatingBarValue);
 
             // 예시: 리뷰 업데이트를 위한 메서드 호출 (실제로는 서버 요청 보내는 방법에 따라 구현 필요)
-            updateReviewOnServer(storeId, reviewId, contentText, updatedRatingBarValue, selectedImageUris);
+            if(isModify){
+                Log.d("LOGAPIisModify", "리뷰 수정");
+                updateReviewOnServer(storeId, reviewId, contentText, updatedRatingBarValue, selectedImageUris);
+            }else {
+                Log.d("LOGAPIisModify", "리뷰 새로 작성");
+                writeReviewOnServer(storeId, contentText, updatedRatingBarValue, selectedImageUris);
+            }
 
             goBack();
-//            moveToCommunityFragment();
 
         });
-
-
 
     }
 
@@ -223,8 +231,7 @@ public class WritingReviewFragment extends Fragment {
         dialog.show();
     }
 
-    private void updateReviewOnServer(Long storeId, Long reviewId, String content, float rating, List<String> imageUris) {
-        // TODO: 서버에 리뷰 업데이트 요청 보내는 로직을 구현하세요.
+    private void updateReviewOnServer(Long storeId, Long reviewId, String content, float rating, List<String> imageUris) {// TODO: 서버에 리뷰 업데이트 요청 보내는 로직을 구현하세요.
         Log.d("LOGAPIupdate!", ""+storeId+", "+reviewId+", "+content+", "+rating+", "+imageUris);
 
         ReviewModifyrRequestDTO requestDTO = new ReviewModifyrRequestDTO();
@@ -232,6 +239,16 @@ public class WritingReviewFragment extends Fragment {
         requestDTO.setStars((int) rating); // float 값을 Integer로 변환
         requestDTO.setImages(imageUris);
         viewModel.ReviewModifyData(storeId, reviewId, requestDTO);
+    }
+
+    private void writeReviewOnServer(Long storeId, String content, float rating, List<String> imageUris) {
+        Log.d("LOGAPIupdate!", ""+storeId+", "+reviewId+", "+content+", "+rating+", "+imageUris);
+
+        ReviewRegisterRequestDTO requestDTO = new ReviewRegisterRequestDTO();
+        requestDTO.setContent(content);
+        requestDTO.setStars((int) rating); // float 값을 Integer로 변환
+        requestDTO.setImages(imageUris);
+        viewModel.ReviewRegisterData(storeId, requestDTO);
     }
 
 
