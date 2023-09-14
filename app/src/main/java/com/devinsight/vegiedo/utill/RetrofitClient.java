86bcefa6d518db905b7c6b0b5731fbf72326c335
iroeun <mange0420@gmail.com>
2023-09-14 23:47:12 +0900

    Feat : post 수정 클릭 시 이미지 재 선택 가능 기능

--- app/src/main/java/com/devinsight/vegiedo/utill/RetrofitClient.java
+++ app/src/main/java/com/devinsight/vegiedo/utill/RetrofitClient.java
@@ -29,11 +29,11 @@ public class RetrofitClient {
     }
 
     public static UserApiService getUserApiService(){
-        return getRetrofit("http://3.38.23.253:8080/test/").create(UserApiService.class);
+        return getRetrofit("http://18.119.70.148:8080/").create(UserApiService.class);
     }
 
     public static StoreApiService getStoreApiService(){
-        return getRetrofit("http://3.38.23.253:8080/").create(StoreApiService.class);
+        return getRetrofit(BASE_URL).create(StoreApiService.class);
     }
 
     public static PostApiService getPostApiService(){
@@ -49,7 +49,7 @@ public class RetrofitClient {
     }
 
     public static MapApiService getMapApiService(){
-        return getRetrofit("http://3.38.23.253:8080/").create(MapApiService.class);
+        return getRetrofit(BASE_URL).create(MapApiService.class);
     }
 
 
